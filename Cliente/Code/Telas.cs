98d4aada0cf57924061388c6e3f2b3077d2d44af
matheusluz96 <matheusluz96@gmail.com>
2016-05-01 03:06:37 -0300

    Ué

---
+++ Cliente/Code/Telas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APS_5.Forms;
+
+namespace APS_5.Code
+{
+    public static class Telas
+    {
+        public static TelaInicial telaInicial;
+        public static TelaChat telaChat;
+        public static TelaChat telaMenu;
+        public static TelaChat telaOnline;
+
+    }
+}
---
+++ Cliente/Forms/FormBridge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APS_5.Forms
+{
+    public class FormBridge : Form
+    {
+        private bool continuarExecucao = false;
+        protected void Close(bool continuarExecucaoTemp)
+        {
+            continuarExecucao = continuarExecucaoTemp;
+            base.Close();
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (!continuarExecucao)
+                Application.Exit();
+        }
+    }
+}
