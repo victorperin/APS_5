40c223a7fcc6ee221efddca0a30b345d8ec59b31
matheusluz96 <matheusluz96@gmail.com>
2016-05-01 06:05:47 -0300

    Corrigindo cores

--- Cliente/Forms/TelaInicial.Designer.cs
+++ Cliente/Forms/TelaInicial.Designer.cs
@@ -38,7 +38,8 @@ namespace APS_5.Forms
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(101, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 38);
@@ -58,9 +59,10 @@ namespace APS_5.Forms
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.Size = new System.Drawing.Size(173, 22);
             this.label1.TabIndex = 2;
             this.label1.Text = "Escolha um nome:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -69,6 +71,7 @@ namespace APS_5.Forms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(278, 323);
             this.Controls.Add(this.label1);
