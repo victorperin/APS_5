9bd1cb03911f27ca822c0fa1e39d19731f483547
matheusluz96 <matheusluz96@gmail.com>
2016-05-01 05:59:31 -0300

    Tratamento de erros e correções

--- Cliente/Forms/TelaInicial.Designer.cs
+++ Cliente/Forms/TelaInicial.Designer.cs
@@ -33,12 +33,13 @@ namespace APS_5.Forms
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelaInicial));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.button1.Location = new System.Drawing.Point(100, 149);
+            this.button1.Location = new System.Drawing.Point(101, 149);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 38);
             this.button1.TabIndex = 0;
@@ -48,11 +49,21 @@ namespace APS_5.Forms
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 51);
+            this.textBox1.Location = new System.Drawing.Point(30, 51);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(217, 26);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Escolha um nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // TelaInicial
             // 
@@ -60,6 +71,7 @@ namespace APS_5.Forms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(278, 323);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -78,6 +90,7 @@ namespace APS_5.Forms
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
